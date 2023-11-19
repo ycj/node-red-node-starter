29c69a25585205415cbbb8131a86bc60b1ad2520
ycj <108540613@qq.com>
2023-11-19 17:12:01 +0800

    单元测试OK

--- test/lower-case.spec.js
+++ test/lower-case.spec.js
@@ -1,22 +1,19 @@
-const helper=require('node-red-node-test-helper')
+const helper = require("node-red-node-test-helper");
 const lowerNode=require('../src/lower-case.js')
 
-describe("单元测试", function() {
-  it("字符串转小写", function() {
-    "Hello".toLowerCase().should.equal("hello");
-  })
-})
+helper.init(require.resolve('node-red'));
 
 describe('lower-case Node', function() {
-  // beforeEach(function(done) {
-  //   helper.startServer(done);
-  // });
-
-  afterEach(function(done) {
-    helper.unload();
+  beforeEach(function(done) {
+    helper.startServer(done);
   });
 
-  it('加载组件', function(done) {
+  afterEach( function(done) {
+    helper.unload();
+    helper.stopServer(done);
+  });
+
+  it('组件加载', function(done) {
     var flow = [{id:"n1", type:"lower-case", name: "lower-case" }];
     helper.load(lowerNode, flow, function() {
       var n1 = helper.getNode("n1");
